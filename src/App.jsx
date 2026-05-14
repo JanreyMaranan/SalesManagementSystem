f8b55cf1b234ee63a817f0f57e93cd8bc98f7c1f
JanReyMaranan <janreymaranan123@gmail.com>
2026-05-15 00:03:13 +0800

    initial project setup

---
+++ src/App.jsx
@@ -0,0 +1,52 @@
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom"
+
+import ProtectedRoute from "./routes/ProtectedRoute"
+
+import Sales from "./pages/Sales"
+import TransNo from "./pages/TransNo"
+import Customers from "./pages/Customers"
+import Employees from "./pages/Employees"
+import Products from "./pages/Products"
+import Prices from "./pages/Prices"
+import Reports from "./pages/Reports"
+import Admin from "./pages/Admin"
+import DeletedItems from "./pages/DeletedItems"
+import AuthCallback from "./pages/AuthCallback"
+import Login from "./pages/Login"
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+
+        <Route path="/login" element={<Login />} />
+
+        <Route
+          path="/sales"
+          element={
+            <ProtectedRoute>
+              <Sales />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route path="/sales/transNo" element={<TransNo />} />
+        <Route path="/lookups/customers" element={<Customers />} />
+        <Route path="/lookups/employees" element={<Employees />} />
+        <Route path="/lookups/products" element={<Products />} />
+        <Route path="/lookups/prices" element={<Prices />} />
+        <Route path="/reports" element={<Reports />} />
+        <Route path="/admin" element={<Admin />} />
+        <Route path="/deleted-items" element={<DeletedItems />} />
+        <Route path="/auth/callback" element={<AuthCallback />} />
+
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
---
+++ src/pages/TransNo.jsx
@@ -0,0 +1,3 @@
+export default function TransNo() {
+  return <h1>Page</h1>
+}
