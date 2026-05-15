e36f212d2615b02ef7ebbd3601043fdd8444c405
JanReyMaranan <janreymaranan123@gmail.com>
2026-05-15 13:26:12 +0800

    add error and loading states

---
+++ src/components/ErrorBoundary.tsx
@@ -0,0 +1,5 @@
+function ErrorBoundary() {
+  return <p>Something went wrong.</p>
+}
+
+export default ErrorBoundary
--- src/pages/Sales.jsx
+++ src/pages/Sales.jsx
@@ -1,3 +1,19 @@
-export default function Sales() {
-  return <h1>Page</h1>
+import Loading from "../components/Loading"
+import ErrorBoundary from "../components/ErrorBoundary"
+
+function Sales() {
+
+  const loading = false
+  const error = false
+
+  if (loading) return <Loading />
+  if (error) return <ErrorBoundary />
+
+  return (
+    <div>
+      <h1>Sales Page</h1>
+    </div>
+  )
 }
+
+export default Sales
